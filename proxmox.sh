405138aad3b527c5b23ff986d1eb4d128bf7ad71
4Peek <69076282+4Peek@users.noreply.github.com>
2024-04-05 00:10:48 +0300

    Update proxmox.sh

--- proxmox.sh
+++ proxmox.sh
@@ -5,11 +5,22 @@ apt install smbclient -y
 read -p "Введите ip файлового хранилиша: " SAMBA
 read -p "Введите имя вашего локального хранилища: " STORAGE
 
-smbclient //$SAMBA/diskfiles -N -c "get alt.vmdk alt.vmdk"
-echo "Alt disk downloaded"
-qm create 100 --name "Alt" --cores 3 --memory 4196 --ostype l26 --scsihw virtio-scsi-single  --net0 virtio,bridge=vmbr0 --net1 virtio,bridge=vmbr1 --net2 virtio,bridge=vmbr2
-qm importdisk 100 alt.vmdk $STORAGE --format qcow2 
-echo "Alt disk imported"
+smbclient //$SAMBA/diskfiles -N -c "get ISP-disk001.vmdk ISP-disk001.vmdk"
+echo "ISP disk downloaded"
+qm create 100 --name "ISP" --cores 1 --memory 1024 --ostype l26 --scsihw virtio-scsi-single  --net0 virtio,bridge=vmbr0 --net1 virtio,bridge=vmbr1 --net2 virtio,bridge=vmbr2
+qm importdisk 100 ISP-disk001.vmdk $STORAGE --format qcow2 
+echo "ISP disk imported"
 qm set 100 -ide0 $STORAGE:vm-100-disk-0 --boot order=ide0
-echo "Alt is done!!!"
+echo "ISP is done!!!"
 
+smbclient //$SAMBA/diskfiles -N -c "get RTR-HQ-disk001.vmdk RTR-HQ-disk001.vmdk"
+echo "RTR-HQ disk downloaded"
+qm create 101 --name "RTR-HQ" --cores 4 --memory 4096 --ostype l26 --scsihw virtio-scsi-single --net0 e1000,bridge=vmbr1 --net1 e1000,bridge=vmbr3 
+qm importdisk 101 RTR-HQ-disk001.vmdk $STORAGE --format qcow2 
+echo "RTR-HQ disk imported"
+qm set 101 -ide0 $STORAGE:vm-101-disk-0 --boot order=ide0
+echo "RTR-HQ is done!!!"
+
+smbclient //$SAMBA/diskfiles -N -c "get RTR-BR-disk001.vmdk RTR-BR-disk001.vmdk"
+echo "RTR-BR disk downloaded"
+qm create 102 --name "RTR-BR" --cores 4 --memory 4096 --ostype l26 --scsihw virtio-scsi-single  --net0 e1000,bridge=vmbr2 --net1 e1000,bridge=vmbr4
