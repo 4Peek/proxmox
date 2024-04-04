b3e4f348149d22581a16c577d601739dfa08a637
4Peek <69076282+4Peek@users.noreply.github.com>
2024-04-04 23:37:53 +0300

    Update proxmox.sh

--- proxmox.sh
+++ proxmox.sh
@@ -5,10 +5,10 @@ apt install smbclient -y
 read -p "Введите ip файлового хранилиша: " SAMBA
 read -p "Введите имя вашего локального хранилища: " STORAGE
 
-smbclient //$SAMBA/diskfiles -N -c "get ISP-disk001.vmdk ISP-disk001.vmdk"
+smbclient //$SAMBA/diskfiles -N -c "get alt.vmdk alt.vmdk"
 echo "Alt disk downloaded"
-qm create 100 --name "ISP" --cores 3 --memory 4196 --ostype l26 --scsihw virtio-scsi-single  --net0 virtio,bridge=vmbr0 --net1 virtio,bridge=vmbr1 --net2 virtio,bridge=vmbr2
-qm importdisk 100 ISP-disk001.vmdk $STORAGE --format qcow2 
+qm create 100 --name "Alt" --cores 3 --memory 4196 --ostype l26 --scsihw virtio-scsi-single  --net0 virtio,bridge=vmbr0 --net1 virtio,bridge=vmbr1 --net2 virtio,bridge=vmbr2
+qm importdisk 100 alt.vmdk $STORAGE --format qcow2 
 echo "Alt disk imported"
 qm set 100 -ide0 $STORAGE:vm-100-disk-0 --boot order=ide0
 echo "Alt is done!!!"
