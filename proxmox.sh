6c2b4f8fc672df195fb2cd117759ca829d3eff79
4Peek <69076282+4Peek@users.noreply.github.com>
2024-04-04 19:10:34 +0300

    Update proxmox.sh

--- proxmox.sh
+++ proxmox.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 cat ./ProxmoxInterfaces.txt >> /etc/network/interfaces;
 systemctl restart networking;
+apt install smbclient -y
 read -p "Введите ip файлового хранилиша: " SAMBA
 read -p "Введите имя вашего локального хранилища: " STORAGE
-curl -u "domain\username:passwd" smb://$SAMBA/diskfiles/ISP-disk001.vmdk -o ISP-disk001.vmdk
+smbclient //$SAMBA/diskfiles -N -c "get ISP-disk001.vmdk ISP-disk001.vmdk"
 qm create 100 --name "ISP" --cores 1 --memory 1024 --ostype l26 --scsihw virtio-scsi-single  --net0 virtio,bridge=vmbr0 --net1 virtio,bridge=vmbr1 --net2 virtio,bridge=vmbr2
 qm importdisk 100 ISP-disk001.vmdk $STORAGE --format qcow2 
 qm set 100 -ide0 $STORAGE:vm-100-disk-0 --boot order=ide0
